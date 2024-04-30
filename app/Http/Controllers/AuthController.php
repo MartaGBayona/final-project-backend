591329d60b6d047fedd968a6b4af595dcc7b6cea
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-04-30 15:40:21 +0200

    feat: create function loggin successfully

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use PhpParser\Node\Stmt\TryCatch;
 
 class AuthController extends Controller
 {
@@ -64,4 +66,74 @@ class AuthController extends Controller
             );
         }
     }
+
+    public function login(Request $request)
+    {
+        try {
+            $email = $request->input('email');
+            $password = $request->input('password');
+
+            $validator = validator::make($request->all(), [
+                'email' => 'required',
+                'password' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    "success" => false,
+                    "message" => "Validation failed",
+                    "error" => $validator->errors()
+                ]);
+            }
+
+            $user = User::query()->where('email', $email)->first();
+
+            if (!$user) {
+                return response()->json(
+                    [
+                        'success' => true,
+                        'message' => "User cant be logging",
+                        'data' => $user
+                    ],
+                    400
+                );
+            }
+
+            $checkPasswordUser = Hash::check($password, $user->password);
+
+            if (!$checkPasswordUser) {
+                return response()->json(
+                    [
+                        "success" => false,
+                        "message" => "Email or password not valid 2",
+                        // "error" => $th->getMessage()
+                    ],
+                    400
+                );
+            }
+
+            $token = $user->createToken('api-token')->plainTextToken;
+
+            return response()->json(
+                [
+                    "success" => true,
+                    "message" => "user logged successfully",
+                    "token" => $token
+                ],
+                200
+            );
+
+        } catch (\Throwable $th) {
+            Log::error($th->getMessage());
+
+            return response()->json(
+                [
+                    'success'=> false,
+                    'message'=> "User cant be logged"
+                ],
+                500
+            );
+        }
+    }
 };
+

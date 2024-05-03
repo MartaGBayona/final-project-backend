466ea55c482ea56c880859a95e8b8ece4b6150d5
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-03 14:44:35 +0200

    feat: create function getAllInscriptions successfully

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inscription;
 use Illuminate\Http\Request;
 
 class InscriptionController extends Controller
 {
-    //
+    public function getAllInscriptions()
+    {
+        try {
+            $inscriptions = Inscription::with('student', 'course')->get();
+
+            return response()->json([
+                "success" => true,
+                "message" => "All relations between rooms and users retrieved successfully",
+                "data" => $inscriptions
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "success" => false,
+                "message" => "Relations not retrieved",
+                "error" => $th->getMessage()
+            ], 500);
+        }
+    }
 }
--- routes/inscriptions.php
+++ routes/inscriptions.php
@@ -1,5 +1,11 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\InscriptionController;
+use App\Http\Middleware\IsDirector;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/inscriptions', [InscriptionController::class, 'getAllInscriptions'])->middleware(['auth:sanctum', IsDirector::class]);
+// Route::get('/inscriptions', [InscriptionController::class, 'getAllMyInscriptions'])->middleware('auth:sanctum');
+Route::post('/inscriptions', [InscriptionController::class, 'postInscriptions'])->middleware('auth:sanctum');
+Route::delete('/insrciption/{id}', [InscriptionController::class, 'deleteInscription'])->middleware('auth:sanctum');

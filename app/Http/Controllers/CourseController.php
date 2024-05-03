3d6c0bcf99b5155da0cab0592acef9c378b8f20d
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-03 08:47:29 +0200

    feat: create function getAllCourses successfully

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    public function getAllCourses ()
+    {
+        try {
+            $courses = Course::all();
+            return response()->json(
+                [
+                    'success'=> true,
+                    'message' => "Courses retrieved successfully",
+                    'data' => $courses
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json()(
+                [
+                    'success'=> false,
+                    'message' => "Courses cant be retrieved",
+                    'error' => $th->getMessage()
+                ],
+                500
+            );
+        }
+    }
 }
--- routes/courses.php
+++ routes/courses.php
@@ -1,5 +1,7 @@
 <?php
 
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/courses', [CourseController::class, 'getAllCourses']);

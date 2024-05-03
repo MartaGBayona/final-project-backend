732a57eae440a467e82d3a405477ca67e01f7c4d
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-03 09:35:31 +0200

    feat: create function postCourse successfully

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -30,4 +30,31 @@ class CourseController extends Controller
             );
         }
     }
+
+    public function postCourse (Request $request) {
+        try {
+            $course = new Course;
+            $course->title = $request->input('title');
+            $course->description = $request->input('description');
+            $course->save();
+
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => "Course create successfully",
+                    'data' => $course
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json()(
+                [
+                    'success'=> false,
+                    'message' => "Course cant be created",
+                    'error' => $th->getMessage()
+                ],
+                500
+            );
+        }
+    }
 }
--- routes/courses.php
+++ routes/courses.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\CourseController;
+use App\Http\Middleware\IsDirector;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/courses', [CourseController::class, 'getAllCourses']);
+Route::post('/courses', [CourseController::class, 'postCourse'])->middleware(['auth:sanctum', IsDirector::class]);

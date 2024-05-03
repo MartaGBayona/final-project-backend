45332abe954ebfbdb8e9c9d0700110ba84981824
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-03 15:41:59 +0200

    feat: create function postInscription successfully

--- app/Http/Controllers/InscriptionController.php
+++ app/Http/Controllers/InscriptionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use App\Models\Inscription;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -65,6 +66,67 @@ class InscriptionController extends Controller
                     "success" => false,
                     "message" => "Inscriptions not retrieved",
                     "error" => $th->getMessage()
+                ],
+                500
+            );
+        }
+    }
+
+    public function postInscription (Request $request, $courseId) {
+        try {
+            $course = Course::find($courseId);
+            if (!$course) {
+                return response()->json([
+                    'success' => false,
+                    'message' => "Course not found",
+                ], 404);
+            }
+
+            $authenticatedUser = Auth::user();
+            if ($authenticatedUser->role_id == 1) {
+            } else {
+                if ($authenticatedUser->id != $request->input('student_id')) {
+                    return response()->json([
+                        'success' => false,
+                        'message' => "You can only enroll yourself",
+                    ], 403);
+                }
+            }
+
+            $validatedData = $request->validate([
+                'student_id' => 'required|exists:users,id',
+            ]);
+
+            $existingInscription = Inscription::where('student_id', $validatedData['student_id'])
+            ->where('course_id', $courseId)
+            ->exists();
+
+        if ($existingInscription) {
+            return response()->json([
+                'success' => false,
+                'message' => "User is already enrolled in this course",
+            ], 400);
+        }
+
+            $inscription = new Inscription;
+            $inscription->student_id = $request->input('student_id');
+            $inscription->course_id = $courseId;
+            $inscription->save();
+
+            return response()->json(
+                [
+                    'success' => true,
+                    'message' => "Inscription create successfully",
+                    'data' => $inscription
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json(
+                [
+                    'success'=> false,
+                    'message' => "Inscription cant be created",
+                    'error' => $th->getMessage()
                 ],
                 500
             );
--- routes/inscriptions.php
+++ routes/inscriptions.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/inscriptions', [InscriptionController::class, 'getAllInscriptions'])->middleware(['auth:sanctum', IsDirector::class]);
 Route::get('/users/{userId}/inscriptions', [InscriptionController::class, 'getUserInscriptions'])->middleware('auth:sanctum');
-Route::post('/inscriptions', [InscriptionController::class, 'postInscriptions'])->middleware('auth:sanctum');
+Route::post('/courses/{courseId}/inscriptions', [InscriptionController::class, 'postInscription'])->middleware('auth:sanctum');
 Route::delete('/insrciption/{id}', [InscriptionController::class, 'deleteInscription'])->middleware('auth:sanctum');

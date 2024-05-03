d268de5c95d09c1c024b4225783ed0de08d4bf65
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-03 13:21:04 +0200

    feat: create function getAllSubjets successfully

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Subject;
 use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
-    //
+    public function getAllSubjects ()
+    {
+        try {
+            $subjects = Subject::all();
+            return response()->json(
+                [
+                    'success'=> true,
+                    'message' => "Courses retrieved successfully",
+                    'data' => $subjects
+                ],
+                200
+            );
+        } catch (\Throwable $th) {
+            return response()->json()(
+                [
+                    'success'=> false,
+                    'message' => "Courses cant be retrieved",
+                    'error' => $th->getMessage()
+                ],
+                500
+            );
+        }
+    }
 }
--- routes/subjects.php
+++ routes/subjects.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\UserController;
+use App\Http\Middleware\IsDirector;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/subjects', [SubjectController::class, 'getAllSubjects']);
+Route::post('/subjects', [SubjectController::class, 'postCourse'])->middleware(['auth:sanctum', IsDirector::class]);
+Route::put('/subjects/{id}', [SubjectController::class, 'putCourse'])->middleware(['auth:sanctum', IsDirector::class]);
+Route::delete('/subjects/{id}', [SubjectController::class, 'deleteCourse'])->middleware(['auth:sanctum', IsDirector::class]);
+Route::get('/subjects/{id}', [SubjectController::class, 'getCourseById']);

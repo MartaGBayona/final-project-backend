803576671b767ac50e428324265a97ea0e55161f
Marta Gimeno Bayona <marta.g.bayona@outlook.es>
2024-05-10 10:03:02 +0200

    update: modified rute deleteInscriptions

--- routes/inscriptions.php
+++ routes/inscriptions.php
@@ -8,4 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/inscriptions', [InscriptionController::class, 'getAllInscriptions'])->middleware(['auth:sanctum', IsDirector::class]);
 Route::get('/users/{userId}/inscriptions', [InscriptionController::class, 'getUserInscriptions'])->middleware('auth:sanctum');
 Route::post('/courses/{courseId}/inscriptions', [InscriptionController::class, 'postInscription'])->middleware('auth:sanctum');
-Route::delete('/insrciptions/{id}', [InscriptionController::class, 'deleteInscription'])->middleware('auth:sanctum');
+Route::delete('insrciptions/{id}', [InscriptionController::class, 'deleteInscription'])->middleware('auth:sanctum');
